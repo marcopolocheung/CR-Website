1f97e9b7f8fa6d2f4e87d136600f678a5e9b46b4
marcopolocheung <marcoctpolo@gmail.com>
2026-03-31 08:38:48 -0500

    add config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,5 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   basePath: isProd ? '/CR-Website' : '',
   assetPrefix: isProd ? '/CR-Website/' : '',
+  output: 'export', 
 };
